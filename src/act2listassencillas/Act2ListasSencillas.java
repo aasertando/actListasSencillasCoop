44bc5001e6ed59518dbba6438d2083e61135eaab
aser <aser@Linux-Aser>
2026-09-10 16:01:49 -0500

    subida de archivos iniciales

---
+++ src/act2listassencillas/Act2ListasSencillas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package act2listassencillas;
+
+/**
+ *
+ * @author aser
+ */
+public class Act2ListasSencillas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
